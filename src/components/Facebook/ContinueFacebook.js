e54747c804be1ba9a5491bfcf641df690ce5fb89
Inti_Rojas <intirraptor1@gmail.com>
2023-07-28 07:19:16 -0400

    guardar cambios git cherry-pick --continue

--- src/components/Facebook/ContinueFacebook.js
+++ src/components/Facebook/ContinueFacebook.js
@@ -2,63 +2,23 @@ import * as React from 'react';
 
 import { _authLogin } from '../../redux/actions/authActions'
 import AlertStyled from '../ui/AlertStyled'
-import { Modal, ActivityIndicator, Alert } from 'react-native';
+import { Modal, ActivityIndicator } from 'react-native';
 import ViewStyled from '../ui/ViewStyled';
 import TextStyled from '../ui/TextStyled';
 import { theme } from '../../utils/theme';
 import ButtonAuthentication from '../Btn/ButtonAuthentication';
 
-import app from '../../../firebaseSetup';
-import { getAuth, FacebookAuthProvider, signInWithCredential } from 'firebase/auth'
-import { LoginManager, AccessToken, AuthenticationToken, Settings, LoginButton } from 'react-native-fbsdk-next'
 
 export default function ContinueFacebook({ display }) {
     const [text, setText] = React.useState('')
     const [Loading, setLoading] = React.useState(false)
-    const auth = getAuth(app)
-
-    const SignInWithFB = async () => {
-        const result = await LoginManager.logInWithPermissions(["public_profile", "email", "user_photos", "user_gender"])
-
-        if (result.isCancelled) {
-            console.log("Login cancelled");
-            return;
-        } else {
-            console.log('Result LoginManager: ', result)
-        }
-
-        const data = await AccessToken.getCurrentAccessToken()
-        console.log('Data: ', data)
-
-        if (!data) {
-            console.log('Something went wrong obtaining access token.')
-        }
-
-        const credential = FacebookAuthProvider.credential(data.accessToken);
-        const userCredential = await signInWithCredential(auth, credential);
-        const user = userCredential.user;
-        const birthdayUser = await userCredential._tokenResponse.dateOfBirth;
-        console.log('Credential: ', userCredential);
-
-        // Extract user information
-        const email = user.email || 'N/A';
-        const birthday = birthdayUser || 'N/A';
-        const firstName = user.providerData[0].displayName.split(' ')[0] || 'N/A';
-        const lastName = user.providerData[0].displayName.split(' ')[1] || 'N/A';
-
-        // Display user information in an alert
-        Alert.alert(
-            'User Information',
-            `Email: ${email}\nBirthday: ${birthday}\nFirst Name: ${firstName}\nLast Name: ${lastName}`
-        );
-    }
 
     return (
         <>
             <ButtonAuthentication
                 shadow={false}
                 title={"Continuar con Facebook"}
-                onPress={SignInWithFB}
+                onPress={() => null}
                 backgroundColor={theme.dark}
                 WithBorder={true}
                 borderColor={theme.tertiary}
--- src/screens/WalletScreen.js
+++ src/screens/WalletScreen.js
@@ -588,7 +588,6 @@ export default function WalletScreen({ route }) {
               </Animated.View>
             </ViewStyled>
           </Animated.ScrollView>
-
           <Animated.View
             style={{
               transform: [
